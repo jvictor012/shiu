13987843dc1d9366185e9d63f26420defadea499
Victor <joaovictorqueimadas@gmail.com>
2026-04-17 14:37:42 -0300

    estrutura base

---
+++ main.py
@@ -0,0 +1,6 @@
+from flask import flask
+from app import app
+import rotas
+
+if __name__== "__main__":
+    app.run(debug=True)
---
+++ rotas.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    pass
+
+@app.route('/testar')
+def teste():
+    pass
