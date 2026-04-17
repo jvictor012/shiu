a865909342b4aa338361c29c6d30a8174e200790
Victor <joaovictorqueimadas@gmail.com>
2026-04-17 15:38:40 -0300

    adicionando estrutura base de audio(1/3 feito)

--- rotas.py
+++ rotas.py
@@ -3,8 +3,10 @@ from flask import render_template
 
 @app.route('/')
 def home():
+    return render_template("homepage.html")
     pass
 
 @app.route('/testar')
 def teste():
+    return render_template("teste.html")
     pass
---
+++ static/script/main.js
@@ -0,0 +1,10 @@
+let audioContext;
+
+async function Iniciar_captura() {
+    if (audioContext){
+        console.warn("Áudio já ativo!");//evita que comece a gravação duas vezes
+        return
+    }
+
+    
+}
